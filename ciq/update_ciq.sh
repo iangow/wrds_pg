61fb2ecbc2d77b1eadead50e9e17eee452795db9
Ian Gow <iandgow@gmail.com>
2015-09-28 13:47:53 -0400

    Added tables to Capital IQ script.

--- ciq/update_ciq.sh
+++ ciq/update_ciq.sh
@@ -3,6 +3,9 @@
 ./wrds_update.pl --fix-missing ciq.wrds_cusip;
 ./wrds_update.pl --fix-missing ciq.wrds_cik;
 ./wrds_update.pl ciq.wrds_keydev
+./wrds_update.pl ciq.wrds_professional
+./wrds_update.pl ciq.ciqkeydeveventtype
+./wrds_update.pl ciq.ciqkeydevobjectroletype
 
 if [ $? -eq 1 ] ; then
     psql -f ciq/ciq_indexes.sql
