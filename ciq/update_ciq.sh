351a17b4684352916a08ef81d30df11543966e07
Ian Gow <igow@hbs.edu>
2016-03-23 16:33:51 -0400

    Address carriage returns in ciq.wrds_professional.

--- ciq/update_ciq.sh
+++ ciq/update_ciq.sh
@@ -3,7 +3,7 @@
 ./wrds_update.pl --fix-missing ciq.wrds_cusip;
 ./wrds_update.pl --fix-missing ciq.wrds_cik;
 ./wrds_update.pl ciq.wrds_keydev
-./wrds_update.pl ciq.wrds_professional
+./wrds_update.pl ciq.wrds_professional --fix-cr
 ./wrds_update.pl ciq.ciqkeydeveventtype
 ./wrds_update.pl ciq.ciqkeydevobjectroletype
 
