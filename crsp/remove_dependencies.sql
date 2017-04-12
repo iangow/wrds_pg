c3ac2da79565904d85399da3fef0fcc01154e8a6
Ian Gow <iandgow@gmail.com>
2017-04-12 09:19:49 -0400

    Remove code related to dependencies.

--- crsp/remove_dependencies.sql
+++
@@ -1,6 +0,0 @@
--- DROP TABLE crsp.dsedist;
-DROP VIEW activist_director.director_bio_matched;
-DROP VIEW targeted.sharkwatch;
-DROP VIEW activist_director.sharkwatch;
-DROP VIEW activist_director.permnos;
-DROP VIEW activism.permnos;
