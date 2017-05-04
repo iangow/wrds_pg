d1722ff15c6d3f9623a92201d80c7a81a9f0cb70
Ian Gow <igow@hbs.edu>
2017-05-04 08:55:49 -0400

    Added code for additional tables to update_comp.py.

--- update_comp.py
+++ update_comp.py
@@ -6,7 +6,7 @@ host = os.getenv("PGHOST", "localhost")
 wrds_id = os.getenv("WRDS_ID")
 dbname = engine = create_engine("postgresql://" + host + "/" + dbname)
 
-from wrds_fetch import wrds_update
+from wrds_fetch import wrds_update, run_file_sql
 
 updated = wrds_update("anncomp", "comp", engine, wrds_id)
 if updated:
@@ -34,3 +34,43 @@ if updated:
 
 updated = wrds_update("idx_index", "comp", engine, wrds_id)
 
+updated = wrds_update("io_qbuysell", "comp", engine, wrds_id)
+if updated:
+    engine.execute("CREATE INDEX ON comp.io_qbuysell (gvkey, datadate)")
+
+updated = wrds_update("wrds_segmerged", "comp", engine, wrds_id)
+if updated:
+    engine.execute("CREATE INDEX ON comp.wrds_segmerged (gvkey, datadate);")
+
+updated = wrds_update("names", "comp", engine, wrds_id)
+if updated:
+    engine.execute("CREATE INDEX ON comp.names (gvkey)")
+
+secm_updated = wrds_update("secm", "comp", engine, wrds_id, force=True)
+if secm_updated:
+    engine.execute("CREATE INDEX ON comp.secm (gvkey, datadate)")
+
+if secm_updated or company_updated:
+    run_file_sql("comp/create_ciks.sql", engine)
+
+updated = wrds_update("secd", "comp", engine, wrds_id)
+if updated:
+    engine.execute("CREATE INDEX ON comp.secd (gvkey, datadate)")
+
+updated = wrds_update("spind_mth", "comp", engine, wrds_id)
+if updated:
+    engine.execute("CREATE INDEX ON comp.spind_mth (gvkey, datadate)")
+
+updated = wrds_update("funda", "comp", engine, wrds_id, fix_missing = True)
+if updated:
+    engine.execute("CREATE INDEX ON comp.funda (gvkey, datadate)")
+
+updated = wrds_update("fundq", "comp", engine, wrds_id, fix_missing = True)
+if updated:
+    engine.execute("CREATE INDEX ON comp.fundq (gvkey, datadate)")
+
+updated = wrds_update("g_sec_divid", "comp", engine, wrds_id, fix_missing = True)
+if updated:
+    engine.execute("CREATE INDEX ON comp.g_sec_divid (gvkey, datadate)")
+
+updated = wrds_update("idxcst_his", "comp", engine, wrds_id, rename="from=fromdt")
