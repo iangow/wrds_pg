9032c04d06df73fbb5bca0ffb36a99e5ca78df03
Ian Gow <igow@hbs.edu>
2017-05-04 08:55:12 -0400

    Minor formatting changes to update_comp.py.

--- update_comp.py
+++ update_comp.py
@@ -9,35 +9,28 @@ dbname = engine = create_engine("postgresql://" + host + "/" + dbname)
 from wrds_fetch import wrds_update
 
 updated = wrds_update("anncomp", "comp", engine, wrds_id)
-
 if updated:
     engine.execute("CREATE INDEX ON comp.anncomp (gvkey)")
-    
-updated = wrds_update("adsprate", "comp", engine, wrds_id)
 
+updated = wrds_update("adsprate", "comp", engine, wrds_id)
 if updated:
     engine.execute("CREATE INDEX ON comp.adsprate (gvkey, datadate)")
-    
-updated = wrds_update("co_hgic", "comp", engine, wrds_id)
 
+updated = wrds_update("co_hgic", "comp", engine, wrds_id)
 if updated:
     engine.execute("CREATE INDEX ON comp.co_hgic (gvkey)")
-    
-updated = wrds_update("co_ifndq", "comp", engine, wrds_id)
 
+updated = wrds_update("co_ifndq", "comp", engine, wrds_id)
 if updated:
     engine.execute("CREATE INDEX ON comp.co_ifndq (gvkey, datadate)")
-    
-updated = wrds_update("company", "comp", engine, wrds_id)
 
-if updated:
+company_updated = wrds_update("company", "comp", engine, wrds_id)
+if company_updated:
     engine.execute("CREATE INDEX ON comp.company (gvkey)")
-    
-updated = wrds_update("idx_ann", "comp", engine, wrds_id)
 
+updated = wrds_update("idx_ann", "comp", engine, wrds_id)
 if updated:
     engine.execute("CREATE INDEX ON comp.idx_ann (datadate)")
-    
+
 updated = wrds_update("idx_index", "comp", engine, wrds_id)
 
-updated = wrds_update("io_qbuysell", "comp", engine, wrds_id)
