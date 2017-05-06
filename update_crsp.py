628700feaa3f975b75cf825b7dc417e383fd93df
Ian Gow <igow@hbs.edu>
2017-05-06 16:02:27 -0400

    Minor fixes to CRSP update code.

--- update_crsp.py
+++ update_crsp.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 from sqlalchemy import create_engine
-import subprocess
 import os
 dbname = os.getenv("PGDATABASE")
 host = os.getenv("PGHOST", "localhost")
 wrds_id = os.getenv("WRDS_ID")
-dbname = engine = create_engine("postgresql://" + host + "/" + dbname)
+engine = create_engine("postgresql://" + host + "/" + dbname)
 
 from wrds_fetch import wrds_update, run_file_sql
 
