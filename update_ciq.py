dbdae17cfec49aec6dba4c61f2ac5c1f9fe1b008
Ian Gow <iandgow@gmail.com>
2017-06-01 11:03:28 -0400

    Fixes to code to update Capital IQ.

--- update_ciq.py
+++ update_ciq.py
@@ -8,24 +8,28 @@ dbname = engine = create_engine("postgresql://" + host + "/" + dbname)
 
 from wrds_fetch import wrds_update
 
-from wrds_fetch import wrds_update
 updated = wrds_update("wrds_gvkey", "ciq", engine, wrds_id, fix_missing=True)
 if updated:
     engine.execute("CREATE INDEX ON ciq.wrds_gvkey (companyid)")
 
-updated= wrds_update("wrds_cusip", "ciq", engine, wrds_id, fix_missing=True)
+updated = wrds_update("wrds_cusip", "ciq", engine, wrds_id, fix_missing=True)
 if updated:
     engine.execute("CREATE INDEX ON ciq.wrds_cusip (companyid)")
-                   
+
 updated = wrds_update("wrds_cik", "ciq", engine, wrds_id, fix_missing=True)
 if updated:
     engine.execute("CREATE INDEX ON ciq.wrds_cik (companyid)")
-                   
-updated = wrds_update("wrds_keydev", "ciq", engine, wrds_id)
+
+updated = wrds_update("wrds_keydev", "ciq", engine, wrds_id, fix_missing=True)
 if updated:
     engine.execute("CREATE INDEX ON ciq.wrds_keydev (keydeveventtypeid)")
     engine.execute("CREATE INDEX ON ciq.wrds_keydev (companyid)")
 
 wrds_update("wrds_professional", "ciq", engine, wrds_id, fix_cr=True)
-wrds_update("ciqkeydevobjectroletype", "ciq", engine, wrds_id)
-wrds_update("ciqkeydeveventtype", "ciq", engine, wrds_id)
+wrds_update("ciqkeydeveventtype", "ciq", engine, wrds_id, fix_cr=True)
+wrds_update("ciqkeydevobjectroletype", "ciq", engine, wrds_id, fix_cr=True)
+wrds_update("ciqfininstance", "ciq", engine, wrds_id)
+wrds_update("ciqfinperiod", "ciq", engine, wrds_id)
+wrds_update("ciqgvkeyiid", "ciq", engine, wrds_id)
+wrds_update("ciqkeydevstatus", "ciq", engine, wrds_id)
+wrds_update("keydev", "ciq", engine, wrds_id)
