c2c1bdfb880c2d76a79845b8d6df269d05978b06
Ian Gow <igow@hbs.edu>
2017-12-27 11:02:30 -0500

    Added corrections to vavoteresults.
    - Fixed variable types.
    - Fixed errors detected in earlier analysis.

--- update_issvoting.py
+++ update_issvoting.py
@@ -11,7 +11,50 @@ engine.execute("GRANT USAGE ON SCHEMA risk TO risk")
 
 from wrds_fetch import wrds_update
 
-wrds_update("vavoteresults", "risk", engine, wrds_id)
+update = wrds_update("vavoteresults", "risk", engine, wrds_id)
+
+if update:
+    sql = """
+        ALTER TABLE risk.vavoteresults ALTER COLUMN companyid TYPE integer;
+        ALTER TABLE risk.vavoteresults ALTER COLUMN meetingid TYPE integer;
+        ALTER TABLE risk.vavoteresults ALTER COLUMN itemonagendaid TYPE integer;
+        ALTER TABLE risk.vavoteresults ALTER COLUMN seqnumber TYPE integer;
+
+        UPDATE risk.vavoteresults
+            SET voterequirement = 0.6667 WHERE voterequirement=66.67;
+
+        UPDATE risk.vavoteresults SET base = 'F+A' WHERE base='F A';
+
+        UPDATE risk.vavoteresults SET base = 'F+A+AB'
+            WHERE base IN ('F A AB', 'F+A+B');
+
+        UPDATE risk.vavoteresults
+            SET (votedfor, votedagainst, votedabstain)=(2224433656, 93561790, 34814753)
+            WHERE itemonagendaid=6019529;
+
+        UPDATE risk.vavoteresults
+            SET (votedfor, votedagainst, votedabstain, ticker)= (10540862,1329889,790539,'KIDE')
+            WHERE itemonagendaid=6039421;
+
+        UPDATE risk.vavoteresults
+            SET voteresult='Pass' WHERE itemonagendaid=7495200;
+
+        UPDATE risk.vavoteresults
+            SET (votedfor, voteresult)=(14830551, 'Pass')
+            WHERE itemonagendaid=6049938;
+
+        -- Source: ../912463/000110465904022325/a04-8530_110q.htm
+        UPDATE risk.vavoteresults
+            SET (votedfor, voteresult)=(30136926, 'Pass')
+            WHERE itemonagendaid=6251118;
+
+        --  Source: ../1050606/000105060603000027/form10q6302003.htm
+        UPDATE risk.vavoteresults
+            SET (votedfor, voteresult)=(70548942, 'Pass')
+            WHERE itemonagendaid=6049746;
+    """
+    engine.execute(sql)
+
 wrds_update("issrec", "risk", engine, wrds_id)
 wrds_update("globalvoteresults", "risk", engine, wrds_id)
 wrds_update("proposals", "risk", engine, wrds_id)
