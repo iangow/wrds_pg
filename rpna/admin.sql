896dd5f2dc6600c3c42731d40a767e5bf941f1f8
Ian Gow <iandgow@gmail.com>
2018-08-13 15:18:09 +1000

    Fixed error in rpna admin.sql.
    - Relates to #39.

--- rpna/admin.sql
+++ rpna/admin.sql
@@ -1,6 +1,6 @@
 -- In R, system("psql -f admin.sql") runs this
-CREATE SCHEMA prna;
-CREATE ROLE prna;
-CREATE ROLE prna_access;
-ALTER SCHEMA prna OWNER TO prna;
-GRANT USAGE ON SCHEMA prna TO prna_access;
+CREATE SCHEMA rpna;
+CREATE ROLE rpna;
+CREATE ROLE rpna_access;
+ALTER SCHEMA rpna OWNER TO rpna;
+GRANT USAGE ON SCHEMA rpna TO rpna_access;
