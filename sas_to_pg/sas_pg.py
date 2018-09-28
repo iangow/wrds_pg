882f6caa15aa1729dd122bf8188a6da660bcb88f
Ian Gow <igow@hbs.edu>
2018-09-27 21:38:54 -0500

    Added test data.
    - Relates to #40.

--- sas_to_pg/sas_pg.py
+++ sas_to_pg/sas_pg.py
@@ -8,13 +8,15 @@ host = os.getenv("PGHOST", "localhost")
 engine = create_engine("postgresql://" + host + "/" + dbname)
 
 # Import sas file to pg
-fpath = '/home/xxx/Downloads'
+fpath = os.path.dirname(os.path.abspath(__file__))
+print("fpath: %s" % fpath)
 # Table_name should be the same as sas file, eg. for admit.sas7bdat, table_name = "admit"
-table_name = 'perms_for_r'
-schema = 'aaer'
+
+table_name = 'msp500'
+schema = 'audit'
 
 #wrds_update(table_name, fpath, schema, engine, wrds_id="", fix_missing=True, fix_cr=True,
 # drop="id name", obs="10", rename="fee=fee_old")
 
-wrds_update_local(table_name, fpath, schema, engine, wrds_id="", fix_missing=False, 
+wrds_update_local(table_name, fpath, schema, engine, wrds_id="", fix_missing=False,
 	fix_cr=False, drop="", obs="", rename="")
