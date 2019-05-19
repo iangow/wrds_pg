4b6746cf479ac31d59215e4baa59f817cadd32a0
Ian Gow <iandgow@gmail.com>
2019-05-20 00:24:29 +1000

    Deleted code supplanted by wrds2pg library.
    - Perl scripts.
    - wrds_fetch.py, make_engine.py.

--- make_engine.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-import os
-dbname = os.getenv("PGDATABASE")
-host = os.getenv("PGHOST", "localhost")
-wrds_id = os.getenv("WRDS_ID")
-engine = create_engine("postgresql://" + host + "/" + dbname)
