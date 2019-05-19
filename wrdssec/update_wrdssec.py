8c19924e03e1d3f545c64ca7b04e737a2825abaa
Ian Gow <iandgow@gmail.com>
2019-05-20 01:14:49 +1000

    Fixes to WRDS SEC code.

--- wrdssec/update_wrdssec.py
+++ wrdssec/update_wrdssec.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
-from sqlalchemy import create_engine
-import os, sys
+from wrds2pg.wrds2pg import wrds_update
 
-from wrds2pg import wrds2pg
-from time import gmtime, strftime
-
-updated = wrds2pg.wrds_update("wciklink_gvkey", "wrdssec")
-updated = wrds2pg.wrds_update("wciklink_names", "wrdssec")
-updated = wrds2pg.wrds_update("wciklink_cusip", "wrdssec", drop="tmatch")
-
+updated = wrds_update("wciklink_gvkey", "wrdssec")
+updated = wrds_update("wciklink_names", "wrdssec")
+updated = wrds_update("wciklink_cusip", "wrdssec", drop="tmatch")
