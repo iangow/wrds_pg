ca0afd5b43f25e5d01262d185ef4287b6a403317
Ian Gow <iandgow@gmail.com>
2019-05-20 04:01:32 +1000

    Fixed Thompson Reuters (tfn) code.
    - Use wrds2pg.
    - Removed unused functions.

--- tfn/update_tfn.py
+++ tfn/update_tfn.py
@@ -1,38 +1,8 @@
 #!/usr/bin/env python3
-from sqlalchemy import create_engine, Boolean, MetaData, Table
-import os, sys
-dbname = os.getenv("PGDATABASE")
-host = os.getenv("PGHOST", "localhost")
-wrds_id = os.getenv("WRDS_ID")
-engine = create_engine("postgresql://" + host + "/" + dbname)
+from wrds2pg.wrds2pg import wrds_update, run_file_sql
+from wrds2pg.wrds2pg import make_engine, wrds_id
 
-from wrds2pg import wrds2pg
-from time import gmtime, strftime
-
-
-def mod_col(column, schema, table, engine):
-    command = "ALTER TABLE " + schema + "." + table + \
-              " ALTER COLUMN " + column + " TYPE boolean USING (" + column + "=1)"
-    engine.execute(command)
-    return column
-
-def is_col_to_bool(engine, schema, table):
-    """
-    This function changes type of columns named "is_" to boolean
-    The table is from PostgreSQL, originally from wrds_id
-    """
-    the_table = Table(table, MetaData(), schema=schema, autoload=True,
-                      autoload_with=engine)
-    columns = the_table.c
-
-    col_lst = [col.name for col in columns
-                  if col.name.startswith("is_") and not isinstance(col.type, Boolean)]
-
-    modify_lst = [mod_col(col, schema, table, engine) for col in col_lst]
-    if modify_lst:
-    	print("Columns changed to boolean", modify_lst)
-
-    return modify_lst
+engine = make_engine()
 
 updated = wrds2pg.wrds_update("amend", "tfn")
 updated = wrds2pg.wrds_update("avgreturns", "tfn")
