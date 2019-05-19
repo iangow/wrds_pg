dddb750c5a04ab800f6dd8bae143bcb45363086a
Ian Gow <iandgow@gmail.com>
2019-05-20 04:21:29 +1000

    Tweaks to CUSIP (cusipm) code.
    - **Not tested**; no current access.

--- cusipm/update_cusipm.py
+++ cusipm/update_cusipm.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-from sqlalchemy import create_engine
-import os, sys
-from wrds2pg import wrds2pg
+from wrds2pg.wrds2pg import wrds_update
 
-updated = wrds2pg.wrds_update("issuer", "cusipm")
-updated = wrds2pg.wrds_update("issue", "cusipm")
+updated = wrds_update("issuer", "cusipm")
+updated = wrds_update("issue", "cusipm")
