4f98e103eb69996fef7bfc9d95bd35be06656ebf
Ian Gow <iandgow@gmail.com>
2019-08-06 18:06:56 +1000

    Fixed tfn.idfhist update code.

--- tfn/update_tfn.py
+++ tfn/update_tfn.py
@@ -4,15 +4,15 @@ from wrds2pg.wrds2pg import make_engine, wrds_id
 
 engine = make_engine()
 
-updated = wrds2pg.wrds_update("amend", "tfn")
-updated = wrds2pg.wrds_update("avgreturns", "tfn")
-updated = wrds2pg.wrds_update("company", "tfn", fix_cr=True)
-updated = wrds2pg.wrds_update("form144", "tfn")
-updated = wrds2pg.wrds_update("header", "tfn")
-# updated = wrds2pg.wrds_update("idfhist", "tfn")
-updated = wrds2pg.wrds_update("idfnames", "tfn")
-updated = wrds2pg.wrds_update("rule10b5", "tfn")
-updated = wrds2pg.wrds_update("table1", "tfn")
-updated = wrds2pg.wrds_update("table2", "tfn")
+updated = wrds_update("amend", "tfn")
+updated = wrds_update("avgreturns", "tfn")
+updated = wrds_update("company", "tfn", fix_cr=True)
+updated = wrds_update("form144", "tfn")
+updated = wrds_update("header", "tfn")
+updated = wrds_update("idfhist", "tfn")
+updated = wrds_update("idfnames", "tfn")
+updated = wrds_update("rule10b5", "tfn")
+updated = wrds_update("table1", "tfn")
+updated = wrds_update("table2", "tfn")
 
 
