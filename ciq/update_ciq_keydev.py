fb059381dd0122a6acbd96876f530516a32ab6f1
Ian Gow <iandgow@gmail.com>
2019-09-16 23:12:11 -0400

    Fixes to Capital IQ keydevs code.

--- ciq/update_ciq_keydev.py
+++ ciq/update_ciq_keydev.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
-from sqlalchemy import create_engine
+from wrds2pg.wrds2pg import make_engine, wrds_update
 
-from wrds2pg.wrds2pg import wrds_update
-
-updated = wrds2pg.wrds_update("wrds_keydev", "ciq", fix_missing=True)
+updated = wrds_update("wrds_keydev", "ciq", fix_missing=True)
 if updated:
     engine = make_engine()
     engine.execute("CREATE INDEX ON ciq.wrds_keydev (keydeveventtypeid)")
     engine.execute("CREATE INDEX ON ciq.wrds_keydev (companyid)")
     engine.dispose()
-    
+
+
+updated = wrds_update("ciqkeydev", "ciq", fix_cr=True)
+if updated:
+    engine = make_engine()
+    engine.execute("CREATE INDEX ON ciq.ciqkeydev (keydevid)")
+    engine.dispose()
+
 wrds_update("ciqkeydeveventtype", "ciq", fix_cr=True)
-wrds_update("ciqkeydev", "ciq", fix_cr=True)
 wrds_update("ciqkeydevstatus", "ciq")
 wrds_update("ciqkeydevobjectroletype", "ciq", fix_cr=True)
-wrds_update("ciqkeydevtoobjecttoeventtype", "ciq")
+updated = wrds_update("ciqkeydevtoobjecttoeventtype", "ciq")
+if updated:
+    engine = make_engine()
+    engine.execute("CREATE INDEX ON ciq.ciqkeydevtoobjecttoeventtype (keydevid)")
+    engine.dispose()
+
