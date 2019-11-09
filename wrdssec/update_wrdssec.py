c7e137d81fca6b4853b01511a2778dd4e25f5a5b
Ian Gow <iandgow@gmail.com>
2019-11-10 09:45:48 +1100

    Tweaks to code for WRDS SEC Analytics.

--- wrdssec/update_wrdssec.py
+++ wrdssec/update_wrdssec.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from wrds2pg.wrds2pg import wrds_update
 
-updated = wrds_update("wciklink_gvkey", "wrdssec")
-updated = wrds_update("wciklink_names", "wrdssec")
-updated = wrds_update("wciklink_cusip", "wrdssec", drop="tmatch")
+wrds_update("wciklink_gvkey", "wrdssec")
+wrds_update("wciklink_names", "wrdssec")
+wrds_update("wciklink_cusip", "wrdssec", drop="tmatch")
