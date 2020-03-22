6c5173559c08adf1490ce3de9ad235b3921ca518
Ian Gow <iandgow@gmail.com>
2020-03-22 12:52:52 -0400

    Fixes for ISS data.

--- risk/update_risk.py
+++ risk/update_risk.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 from wrds2pg.wrds2pg import wrds_update
 
-wrds_update("globalvoteresults", "risk")
-wrds_update("vavoteresults", "risk")
-wrds_update("issrec", "risk")
-wrds_update("directors", "risk")
-wrds_update("gset", "risk")
-wrds_update("votes", "risk")
+wrds_update("voteanalysis_npx", "risk", obs=100000000, force=True)
+# wrds_update("globalvoteresults", "risk")
+# wrds_update("vavoteresults", "risk")
+# wrds_update("issrec", "risk")
+# wrds_update("rmdirectors", "risk")
+# wrds_update("gset", "risk")
+# wrds_update("votes", "risk")
+# wrds_update("voteanalysis_npx", "risk")
+# wrds_update("directors", "risk", drop="votecref")
