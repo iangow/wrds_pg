75a68c0b34559e6b0657df50429e9253598cb0bc
Ian Gow <iandgow@gmail.com>
2020-04-03 10:14:41 -0400

    Restoration of code for risk schema.

--- risk/update_risk.py
+++ risk/update_risk.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 from wrds2pg.wrds2pg import wrds_update
 
-wrds_update("voteanalysis_npx", "risk", obs=100000000, force=True)
-# wrds_update("globalvoteresults", "risk")
-# wrds_update("vavoteresults", "risk")
-# wrds_update("issrec", "risk")
-# wrds_update("rmdirectors", "risk")
-# wrds_update("gset", "risk")
-# wrds_update("votes", "risk")
-# wrds_update("voteanalysis_npx", "risk")
-# wrds_update("directors", "risk", drop="votecref")
+wrds_update("globalvoteresults", "risk")
+wrds_update("vavoteresults", "risk")
+wrds_update("issrec", "risk")
+wrds_update("rmdirectors", "risk")
+wrds_update("gset", "risk")
+wrds_update("votes", "risk")
+wrds_update("voteanalysis_npx", "risk")
+wrds_update("directors", "risk", drop="votecref")
