232300f42761dcfda0cc5dd73157c6290600fb5e
Ian Gow <iandgow@gmail.com>
2020-04-13 08:48:50 +1000

    Removed admin.sql.

--- rpna/admin.sql
+++
@@ -1,6 +0,0 @@
--- In R, system("psql -f admin.sql") runs this
-CREATE SCHEMA rpna;
-CREATE ROLE rpna;
-CREATE ROLE rpna_access;
-ALTER SCHEMA rpna OWNER TO rpna;
-GRANT USAGE ON SCHEMA rpna TO rpna_access;
