5d9136a0fdb9d98939375c8b8995c3a71e337557
Ian Gow <iandgow@gmail.com>
2020-05-15 13:55:56 -0400

    Exclude problematic table from BoardEx.
    - Also added a couple of comments.
    - Som code clean-up too.

--- boardex/update_boardex.py
+++ boardex/update_boardex.py
@@ -8,10 +8,10 @@ from sqlalchemy import create_engine
 wrds_id = getenv("WRDS_ID")
 
 def get_wrds_tables(schema, wrds_id):
-    
+
     from sqlalchemy import MetaData
-    
-    wrds_engine = create_engine("postgresql://%s@wrds-pgdata.wharton.upenn.edu:9737/wrds" % wrds_id, 
+
+    wrds_engine = create_engine("postgresql://%s@wrds-pgdata.wharton.upenn.edu:9737/wrds" % wrds_id,
                                 connect_args = {'sslmode':'require'})
 
     metadata = MetaData(wrds_engine, schema=schema)
@@ -24,18 +24,23 @@ def get_wrds_tables(schema, wrds_id):
 def update_schema(schema, wrds_id):
 
     table_list = get_wrds_tables(schema, wrds_id)
-    
+
     regex_list = ["wrds_company_networks", "company_profile_sr_mgrs",
                   "wrds_dir_profile_emp", "dir_profile_emp",
                   "wrds_dir_profile_all", "wrds_org_composition"]
     regex = "(" + "|".join(regex_list) + ")"
 
     for table in table_list:
+        # These tables are way too big and are redundant
         if re.search("individual_networks", table):
             continue
 
+        # Table doesn't work and seems not to be maintained.
+        if table == "wrds_company_dir_names":
+            continue
+
         fix_missing = re.search(regex, table) is not None
-        wrds_update(table_name=table, schema="boardex", 
+        wrds_update(table_name=table, schema="boardex",
                     wrds_id=wrds_id, fix_missing = fix_missing)
 
 schemas = ["boardex_uk","boardex_row", "boardex_eur", "boardex_na"]
