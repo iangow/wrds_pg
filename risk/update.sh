1fbf511dc3f639021802ae9722d27f71adb10ab3
Ian Gow <iandgow@gmail.com>
2020-05-19 05:19:56 +1000

    Tweaks to risk code.

--- risk/update.sh
+++ risk/update.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-./wrds_update.pl risk.rmdirectors
+echo "Get 'easy' risk data."
+risk/update_issvoting.py
+
+echo "Get 'difficult' risk data."
+echo "$PGHOST"
+Rscript --vanilla risk/get_risk_data.R
