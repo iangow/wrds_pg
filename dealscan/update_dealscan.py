57b6e052c71349e4ae7816a3c7afbce9a42e841c
Ian Gow <iandgow@gmail.com>
2020-05-26 14:15:13 -0400

    Automated list of tables for Dealscan.

--- dealscan/update_dealscan.py
+++ dealscan/update_dealscan.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
-from wrds2pg import wrds_update
+from wrds2pg import wrds_update, wrds_id
+from sqlalchemy import create_engine
 
-dealscan_tables = ["borrowerbase", "company", "currfacpricing", "dealamendment", 
-    "dealpurposecomment", "facility", "facilityamendment", "facilitydates", 
-    "facilityguarantor", "facilitypaymentschedule", "facilitysecurity", 
-    "facilitysponsor", "financialcovenant", "financialratios", "lendershares", 
-    "link_table", "lins", "marketsegment", "networthcovenant", "organizationtype", 
-    "package", "packageassignmentcomment", "performancepricing", 
-    "performancepricingcomments", "sublimits", "dbo_df_fac_dates_data"]
+def get_wrds_tables(schema, wrds_id):
+
+    from sqlalchemy import MetaData
+
+    wrds_engine = create_engine("postgresql://%s@wrds-pgdata.wharton.upenn.edu:9737/wrds" % wrds_id,
+                                connect_args = {'sslmode':'require'})
+
+    metadata = MetaData(wrds_engine, schema=schema)
+    metadata.reflect(schema=schema)
+
+    table_list = [key.name for key in metadata.tables.values()]
+    wrds_engine.dispose()
+    return table_list
+
+dealscan_tables = get_wrds_tables("tr_dealscan", wrds_id)
 
 for table in dealscan_tables:
     wrds_update(table, "dealscan")
