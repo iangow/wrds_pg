3465cc0c70957558e6f54a575e209aad4aa3f564
Ian Gow <iandgow@gmail.com>
2020-08-12 16:27:55 -0400

    Added full IBES update code.

--- ibes/update_ibes_full.py
+++ ibes/update_ibes_full.py
@@ -1,16 +1,46 @@
 #!/usr/bin/env python3
-from wrds2pg import wrds_update, wrds_id
-import wrds
-db = wrds.Connection(wrds_username=wrds_id)
-tables = db.list_tables(library='ibes')
+from wrds2pg import wrds_update, run_file_sql
+from wrds2pg import make_engine, wrds_id
 
-for table in tables:
-    fix_missing = table == "currnew"
-    if table == "det_epsint":
-        col_types = {'usfirm':'boolean',
-                     'anntims':'text'}
-    else:
-        col_types = {'usfirm':'boolean'}
-    wrds_update(table, "ibes",
-                col_types = col_types,
-                fix_missing = fix_missing)
+engine = make_engine()
+
+wrds_update("act_epsus", "ibes")
+wrds_update("actpsumu_epsus", "ibes")
+wrds_update("actu_epsus", "ibes")
+updated = wrds_update("detu_epsus", "ibes")
+if updated:
+    engine.execute("SET maintenance_work_mem='1999MB'")
+    engine.execute("CREATE INDEX ON ibes.detu_epsus (ticker, revdats)")
+    
+updated = wrds_update("det_xepsus", "ibes")
+if updated:
+    engine.execute("CREATE INDEX ON ibes.det_xepsus (ticker, revdats)")
+
+wrds_update("curr", "ibes")    
+wrds_update("det_epsus", "ibes")
+wrds_update("detu_epsint", "ibes")
+wrds_update("id", "ibes")
+wrds_update("idsum", "ibes")
+wrds_update("surpsum", "ibes")
+wrds_update("statsum_epsus", "ibes")
+updated = wrds_update("statsumu_epsus", "ibes")
+if updated:
+    engine.execute("SET maintenance_work_mem='1999MB'")
+    engine.execute("CREATE INDEX ON ibes.statsumu_epsus (ticker, statpers)")
+
+wrds_update("det_guidance", "ibes")
+if updated:
+    engine.execute("SET maintenance_work_mem='1999MB'")
+    engine.execute("CREATE INDEX ON ibes.det_guidance (anndats);")
+    
+wrds_update("det_guidance_ext", "ibes")
+wrds_update("id_guidance", "ibes")
+wrds_update("split_guidance", "ibes")
+wrds_update("stop_epsus", "ibes")
+wrds_update("exc_epsus", "ibes")
+wrds_update("actpsum_epsus", "ibes")
+
+engine.dispose()
+
+import subprocess
+subprocess.call("Rscript --vanilla ibes/get_iclink.R", shell=True)
