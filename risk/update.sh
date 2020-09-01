8ce52103df83eaf0cce805af776d7c1cb7c91ce1
Ian Gow <iandgow@gmail.com>
2020-09-01 13:22:16 -0400

    Tweaks to risk script.

--- risk/update.sh
+++ risk/update.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-echo "Get 'easy' risk data."
-risk/update_issvoting.py
-
-echo "Get 'difficult' risk data."
 echo "$PGHOST"
-Rscript --vanilla risk/get_risk_data.R
+echo "$PGPORT"
+
+risk/update_issvoting.py
 
 Rscript --vanilla risk/create_rm_link.R
 Rscript --vanilla risk/import_manual_names.R
