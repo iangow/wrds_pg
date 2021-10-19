6e593a3f90fdc4bbcc704026346b01483880c56d
Ian Gow <iandgow@gmail.com>
2021-10-19 10:29:00 -0400

    Updated comp/create_ciks.sql.

--- comp/create_ciks.sql
+++ comp/create_ciks.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS comp.ciks;
 
-CREATE TABLE comp.ciks AS 
-    SELECT cusip, cik 
+CREATE TABLE comp.ciks AS
+    SELECT cusip, cik
     FROM (SELECT DISTINCT gvkey, cusip FROM comp.secm) AS a
     INNER JOIN (SELECT DISTINCT gvkey, cik FROM comp.company) AS b
     USING (gvkey);
@@ -9,3 +9,14 @@ CREATE TABLE comp.ciks AS
 ALTER TABLE comp.ciks OWNER TO comp;
 GRANT ALL ON TABLE comp.ciks TO comp;
 GRANT SELECT ON TABLE comp.ciks TO comp_access;
+
+SET timezone TO 'UTC';
+
+DO
+$do$
+BEGIN
+EXECUTE 'COMMENT ON TABLE comp.ciks IS ''CREATED USING comp/create_ciks.sql ON '
+     || to_char(current_timestamp, 'YYYY-MM-DD HH24:MI:SS TZ')
+     || '''';
+END
+$do$
