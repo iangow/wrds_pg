944c90edf2a1f16d91bb7c188f1cecfc77149e5d
Ian Gow <iandgow@gmail.com>
2021-10-21 16:25:05 -0400

    Deleted obsolete indexes file.

--- ciq/ciq_indexes.sql
+++
@@ -1,7 +0,0 @@
-SET maintenance_work_mem='3GB';
-
-CREATE INDEX ON ciq.wrds_keydev (companyid);
-CREATE INDEX ON ciq.wrds_keydev (keydeveventtypeid);
-CREATE INDEX ON ciq.wrds_gvkey  (companyid);
-CREATE INDEX ON ciq.wrds_cusip (companyid);
-CREATE INDEX ON ciq.wrds_cik (companyid);
