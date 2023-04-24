92733dfe8a7a84dc4badb70c5a856c2ceecc8630
Ian Gow <iandgow@gmail.com>
2023-04-25 08:54:59 +1000

    Updated ciq/update_ciq.py to reflect changes to wrds2pg.

--- ciq/update_ciq.py
+++ ciq/update_ciq.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python3
-from wrds2pg import wrds_update, make_engine
+from wrds2pg import wrds_update, make_engine, process_sql
 
 engine = make_engine()
 
 updated = wrds_update("wrds_gvkey", "ciq", fix_missing=True)
 if updated:
-    engine.execute("CREATE INDEX ON ciq.wrds_gvkey (companyid)")
+    process_sql("CREATE INDEX ON ciq.wrds_gvkey (companyid)", engine=engine)
 
 updated = wrds_update("wrds_cusip", "ciq", fix_missing=True)
 if updated:
-    engine.execute("CREATE INDEX ON ciq.wrds_cusip (companyid)")
+    process_sql("CREATE INDEX ON ciq.wrds_cusip (companyid)", engine=engine)
 
 updated = wrds_update("wrds_cik", "ciq", fix_missing=True)
 if updated:
-    engine.execute("CREATE INDEX ON ciq.wrds_cik (companyid)")
+    process_sql("CREATE INDEX ON ciq.wrds_cik (companyid)", engine=engine)
 
 updated = wrds_update("ciqfininstance", "ciq")
 if updated:
-    engine.execute("CREATE INDEX ON ciq.ciqfininstance (financialperiodid)")
-    engine.execute("ANALYZE ciq.ciqfininstance")
+    process_sql("CREATE INDEX ON ciq.ciqfininstance (financialperiodid)", engine=engine)
+    process_sql("ANALYZE ciq.ciqfininstance", engine=engine)
     
 updated = wrds_update("ciqfinperiod", "ciq")
 if updated:
-    engine.execute("CREATE INDEX ON ciq.ciqfinperiod (financialperiodid)")
-    engine.execute("CREATE INDEX ON ciq.ciqfinperiod (companyid)")
-    engine.execute("ANALYZE ciq.ciqfinperiod")
+    process_sql("CREATE INDEX ON ciq.ciqfinperiod (financialperiodid)", engine=engine)
+    process_sql("CREATE INDEX ON ciq.ciqfinperiod (companyid)", engine=engine)
+    process_sql("ANALYZE ciq.ciqfinperiod", engine=engine)
     
 updated = wrds_update("ciqgvkeyiid", "ciq")
 if updated:
-    engine.execute("CREATE INDEX ON ciq.ciqgvkeyiid (relatedcompanyid)")
-    engine.execute("ANALYZE ciq.ciqgvkeyiid")
+    process_sql("CREATE INDEX ON ciq.ciqgvkeyiid (relatedcompanyid)", engine=engine)
+    process_sql("ANALYZE ciq.ciqgvkeyiid", engine=engine)
 
 engine.dispose()
