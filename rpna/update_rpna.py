a15cf22e0a536c57dd362387580a1752a1e36f22
Ian Gow <iandgow@gmail.com>
2023-06-27 15:39:51 -0400

    Fixes to RPNA code.

--- rpna/update_rpna.py
+++ rpna/update_rpna.py
@@ -8,12 +8,17 @@ engine = make_engine()
 now = datetime.datetime.now()
 avail_years = range(2000, now.year+1)
 
-updated = wrds_update("rp_entity_mapping", "rpna")
+updated = wrds_update("rpa_entity_mappings", "rpa")
 
 def update_equities(year):
-    updated = wrds_update("pr_equities_" + str(year), "rpna")
+    updated = wrds_update("rpa_djpr_equities_" + str(year), "rpa",
+                          col_types = {"timestamp_utc": "timestamp", 
+                                       "event_start_date_utc": "timestamp", 
+                                       "event_end_date_utc": "timestamp", 
+                                       "reporting_start_date_utc": "timestamp",
+                                       "reporting_end_date_utc": "timestamp"})
     if updated:
-        engine.execute("CREATE INDEX ON rpna.pr_equities_" + str(year) + " (rp_entity_id, rpna_date_utc)")
+        engine.execute("CREATE INDEX ON rpa.rpa_djpr_equities_" + str(year) + " (rp_entity_id, timestamp_utc)")
     return updated
 
 updated = [ update_equities(year) for year in avail_years]
@@ -23,22 +28,22 @@ def select(cols):
     return sql
 
 def get_sql(year, cols):
-    return(select(cols) + "\nFROM rpna.pr_equities_%s\n" % year)
+    return(select(cols) + "\nFROM rpa.rpa_djpr_equities_%s\n" % year)
 
 if any(updated):
 
-    metadata = MetaData(engine, schema="rpna")
-    metadata.reflect(schema = "rpna")
-    cols = [ column.name for column in metadata.tables['rpna.pr_equities_' + str(max(avail_years))].columns]
+    metadata = MetaData(engine, schema="rpa")
+    metadata.reflect(schema = "rpa")
+    cols = [ column.name for column in metadata.tables['rpa.rpa_djpr_equities_' + str(max(avail_years))].columns]
     sql_parts = [get_sql(year, cols) for year in avail_years]
 
-    sql = "CREATE VIEW rpna.pr_equities AS\n" + 'UNION ALL\n'.join(sql_parts)
+    sql = "CREATE VIEW rpa_djpr_equities AS\n" + 'UNION ALL\n'.join(sql_parts)
 
     conn = engine.connect()
     trans = conn.begin()
-    conn.execute("DROP VIEW IF EXISTS rpna.pr_equities CASCADE")
+    conn.execute("DROP VIEW IF EXISTS rpa.rpa_djpr_equities CASCADE")
     conn.execute(sql)
-    conn.execute("ALTER VIEW rpna.pr_equities OWNER TO rpna")
-    conn.execute("GRANT SELECT ON rpna.pr_equities TO rpna_access")
+    conn.execute("ALTER VIEW rpa.rpa_djpr_equities OWNER TO rpa")
+    conn.execute("GRANT SELECT ON rpa.rpa_djpr_equities TO rpa_access")
     trans.commit()
     conn.close()
