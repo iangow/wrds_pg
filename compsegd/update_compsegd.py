2456170b213f9812c33747d53ecf3d1258ecb187
Ian Gow <iandgow@gmail.com>
2023-09-16 12:00:38 +1000

    Modified location of compsegd to match location in WRDS PG.

--- compsegd/update_compsegd.py
+++ compsegd/update_compsegd.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 from wrds2pg import wrds_update
 
-wrds_update("seg_customer", "compsegd")
-wrds_update("names_seg", "compsegd")
+wrds_update("seg_customer", "comp_segments_hist_daily",
+            sas_schema="compsegd")
+wrds_update("names_seg", "comp_segments_hist_daily",
+            sas_schema="compsegd")
