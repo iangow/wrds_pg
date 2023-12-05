278db1ec33988a58d71b08c0350597ded89ce222
Ian Gow <iandgow@gmail.com>
2023-12-05 15:28:34 -0500

    Added code for audit table.

---
+++ audit/update_acc_oversight.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from wrds2pg import wrds_update, make_engine
+
+# Auditor Changes
+updated = wrds_update("feed55_auditor_ratification", "audit", 
+                      drop="match: prior: closest: ",
+                      col_types = { "auditor_ratification_fkey":"integer",
+                                    "share_class_fkey":"integer",
+                                    "auditor_fkey":"integer",
+                                    "pcaob_registration_number":"integer"},
+                      force = True)
