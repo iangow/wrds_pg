6d5582dd3fa0ee6ca9f25b7c4d5dacad2508d9ae
Ian Gow <iandgow@gmail.com>
2023-12-20 10:17:40 -0500

    Comment-out Audit Analytics until it is fixed.

--- update_wrds.sh
+++ update_wrds.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 printf "Updating Audit Analytics (audit) ...\n"
-audit/update_audit_compliance.py
-audit/update_corporate_legal.py
+# audit/update_audit_compliance.py
+# audit/update_corporate_legal.py
 printf "\nUpdating Capital IQ (ciq) ...\n"
 ciq/update_ciq.py
 printf "\nUpdating CRSP (crsp) ...\n"
