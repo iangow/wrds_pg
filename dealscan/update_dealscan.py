22695bdb373529b93dd4e999fc64df24e2e6d63c
Ian Gow <iandgow@gmail.com>
2023-12-20 10:18:08 -0500

    Update Dealscan import.
     - Dealscan is now One Big Table.

--- dealscan/update_dealscan.py
+++ dealscan/update_dealscan.py
@@ -1,22 +1,4 @@
 #!/usr/bin/env python3
-from wrds2pg import wrds_update, wrds_id
-from sqlalchemy import create_engine
-
-def get_wrds_tables(schema, wrds_id):
-
-    from sqlalchemy import MetaData
-
-    wrds_engine = create_engine("postgresql://%s@wrds-pgdata.wharton.upenn.edu:9737/wrds" % wrds_id,
-                                connect_args = {'sslmode':'require'})
-
-    metadata = MetaData(wrds_engine, schema=schema)
-    metadata.reflect(schema=schema)
-
-    table_list = [key.name for key in metadata.tables.values()]
-    wrds_engine.dispose()
-    return table_list
-
-dealscan_tables = get_wrds_tables("tr_dealscan", wrds_id)
-
+from wrds2pg import wrds_update
 for table in dealscan_tables:
-    wrds_update(table, "dealscan")
+    wrds_update("dealscan", "dealscan")
