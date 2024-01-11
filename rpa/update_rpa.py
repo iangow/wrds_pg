24b1bcfc854399d9d7cf68572e2f23124c0cda72
Ian Gow <iandgow@gmail.com>
2024-01-11 09:08:19 -0500

    Fixes to update_rpa.py.

--- rpa/update_rpa.py
+++ rpa/update_rpa.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-from sqlalchemy import MetaData
-from wrds2pg import wrds_update, run_file_sql, make_engine
+from wrds2pg import wrds_update, run_file_sql, make_engine, process_sql
 import datetime
-
+from wrds2pg import get_pg_tables, get_cols
+import re
+    
 engine = make_engine()
 
 now = datetime.datetime.now()
@@ -12,38 +13,37 @@ updated = wrds_update("rpa_entity_mappings", "rpa")
 
 def update_equities(year):
     updated = wrds_update("rpa_djpr_equities_" + str(year), "rpa",
-                          col_types = {"timestamp_utc": "timestamp",
-                                       "event_start_date_utc": "timestamp", 
-                                       "event_end_date_utc": "timestamp", 
-                                       "reporting_start_date_utc": "timestamp",
-                                       "reporting_end_date_utc": "timestamp"})
+                          col_types = {"timestamp_utc": "timestamptz",
+                                       "rpa_time_utc": "text",
+                                       "event_start_date_utc": "timestamptz", 
+                                       "event_end_date_utc": "timestamptz", 
+                                       "reporting_start_date_utc": "timestamptz",
+                                       "reporting_end_date_utc": "timestamptz"})
     if updated:
-        engine.execute("CREATE INDEX ON rpa.rpa_djpr_equities_" + str(year) + " (rp_entity_id, timestamp_utc)")
+        process_sql("CREATE INDEX ON rpa.rpa_djpr_equities_" + str(year) + " (rp_entity_id, timestamp_utc)",
+                    engine)
     return updated
 
 updated = [ update_equities(year) for year in avail_years]
 
-def select(cols):
-    sql = 'SELECT "' + '", "'.join(cols) + '"'
-    return sql
-
-def get_sql(year, cols):
-    return(select(cols) + "\nFROM rpa.rpa_djpr_equities_%s\n" % year)
-
 if any(updated):
-
-    metadata = MetaData(engine, schema="rpa")
-    metadata.reflect(schema = "rpa")
-    cols = [ column.name for column in metadata.tables['rpa.rpa_djpr_equities_' + str(max(avail_years))].columns]
-    sql_parts = [get_sql(year, cols) for year in avail_years]
-
-    sql = "CREATE VIEW rpa.rpa_djpr_equities AS\n" + 'UNION ALL\n'.join(sql_parts)
-
-    conn = engine.connect()
-    trans = conn.begin()
-    conn.execute("DROP VIEW IF EXISTS rpa.rpa_djpr_equities CASCADE")
-    conn.execute(sql)
-    conn.execute("ALTER VIEW rpa.rpa_djpr_equities OWNER TO rpa")
-    conn.execute("GRANT SELECT ON rpa.rpa_djpr_equities TO rpa_access")
-    trans.commit()
-    conn.close()
+    
+    def select(cols):
+        sql = 'SELECT "' + '", "'.join(cols) + '"'
+        return sql
+    
+    def get_sql(table, cols):
+        return(select(cols) + f"\nFROM {table}\n")
+    
+    tables = [key for key in get_pg_tables("rpa", engine, keys=True)
+              if re.search("rpa_djpr_equities", key)]
+    cols = get_cols(max(tables), "rpa", engine)
+    sql_parts = [get_sql(table, cols) for table in tables]
+    
+    sql =  "DROP VIEW IF EXISTS rpa.rpa_djpr_equities CASCADE;"
+    sql += "CREATE VIEW rpa.rpa_djpr_equities AS\n"
+    sql += "UNION ALL\n".join(sql_parts) + ";"
+    sql += "ALTER VIEW rpa.rpa_djpr_equities OWNER TO rpa;"
+    sql += "GRANT SELECT ON rpa.rpa_djpr_equities TO rpa_access;"
+    
+    process_sql(sql, engine)
