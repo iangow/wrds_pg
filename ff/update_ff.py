edda81f87c98c2bc3037bd0b1bf8fa2499a4cae1
Ian Gow <iandgow@gmail.com>
2024-01-11 09:15:34 -0500

    Simplified update_ff.py.

--- ff/update_ff.py
+++ ff/update_ff.py
@@ -1,22 +1,7 @@
 #!/usr/bin/env python3
-from wrds2pg import wrds_update, wrds_id
-from sqlalchemy import create_engine
+from wrds2pg import wrds_update, get_wrds_tables
 
-def get_wrds_tables(schema, wrds_id):
-
-    from sqlalchemy import MetaData
-
-    wrds_engine = create_engine("postgresql://%s@wrds-pgdata.wharton.upenn.edu:9737/wrds" % wrds_id,
-                                connect_args = {'sslmode':'require'})
-    metadata = MetaData()
-    
-    metadata.reflect(bind=wrds_engine, schema=schema)
-
-    table_list = [key.name for key in metadata.tables.values()]
-    wrds_engine.dispose()
-    return table_list
-
-ff_tables = get_wrds_tables("ff_all", wrds_id)
+ff_tables = get_wrds_tables("ff_all")
 
 for table in ff_tables:
     wrds_update(table, "ff")
