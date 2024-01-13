48b25992312ed7d3b669448b2322956abd0ef8ca
Ian Gow <iandgow@gmail.com>
2024-01-13 16:38:31 -0500

    Simplified audit scripts.
     - Now use features of wrds2pg to achieve same effect.

--- audit/update_acc_oversight.py
+++ audit/update_acc_oversight.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from wrds2pg import wrds_update, make_engine
+from wrds2pg import wrds_update
 
 # Auditor Changes
 updated = wrds_update("feed55_auditor_ratification", "audit", 
