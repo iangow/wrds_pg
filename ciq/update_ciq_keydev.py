8393ac3ef3cddc13d195e80d0d1b1d21649cdc69
Ian Gow <iandgow@gmail.com>
2024-01-13 21:46:11 -0500

    Fixes to CIQ code.
     - Not able to test (no access to data).

--- ciq/update_ciq_keydev.py
+++ ciq/update_ciq_keydev.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-from wrds2pg import make_engine, wrds_update
+from wrds2pg import make_engine, wrds_update, process_sql
 
 updated = wrds_update("wrds_keydev", "ciq", fix_missing=True)
 if updated:
     engine = make_engine()
-    engine.execute("CREATE INDEX ON ciq.wrds_keydev (keydeveventtypeid)")
-    engine.execute("CREATE INDEX ON ciq.wrds_keydev (companyid)")
+    process_sql("CREATE INDEX ON ciq.wrds_keydev (keydeveventtypeid)", engine)
+    process_sql("CREATE INDEX ON ciq.wrds_keydev (companyid)", engine)
     engine.dispose()
 
 
 updated = wrds_update("ciqkeydev", "ciq", fix_cr=True)
 if updated:
     engine = make_engine()
-    engine.execute("CREATE INDEX ON ciq.ciqkeydev (keydevid)")
+    process_sql("CREATE INDEX ON ciq.ciqkeydev (keydevid)", engine)
     engine.dispose()
 
 wrds_update("ciqkeydeveventtype", "ciq", fix_cr=True)
@@ -21,6 +21,6 @@ wrds_update("ciqkeydevobjectroletype", "ciq", fix_cr=True)
 updated = wrds_update("ciqkeydevtoobjecttoeventtype", "ciq")
 if updated:
     engine = make_engine()
-    engine.execute("CREATE INDEX ON ciq.ciqkeydevtoobjecttoeventtype (keydevid)")
+    process_sql("CREATE INDEX ON ciq.ciqkeydevtoobjecttoeventtype (keydevid)",
+                engine)
     engine.dispose()
-
