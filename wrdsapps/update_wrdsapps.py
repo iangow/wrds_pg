ce30c988cf1283a04b0163dc616616dc173c1b37
Ian Gow <iandgow@gmail.com>
2024-02-13 15:17:47 -0500

    Minor edits to wrdsapps code.

--- wrdsapps/update_wrdsapps.py
+++ wrdsapps/update_wrdsapps.py
@@ -3,11 +3,11 @@ from wrds2pg import wrds_update
 
 # updated = wrds_update("firm_ratio", "wrdsapps")
 updated = wrds_update("bondret", "wrdsapps",
-                       col_types= {'t_volume':'text',
-                                   't_dvolume':'text',
-                                   't_spread':'text',
-                                   'yield':'text',
-                                   'ret_eom':'text',
-                                   'ret_l5m':'text',
-                                   'ret_ldm':'text'})
+                       col_types= {'t_volume': 'text',
+                                   't_dvolume': 'text',
+                                   't_spread': 'text',
+                                   'yield': 'text',
+                                   'ret_eom': 'text',
+                                   'ret_l5m': 'text',
+                                   'ret_ldm': 'text'})
 wrds_update("ibcrsphist", "wrdsapps")
