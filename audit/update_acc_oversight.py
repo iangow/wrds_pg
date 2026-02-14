9fe8de10e721180b185b660ddbb0183948abec8a
Ian Gow <iandgow@gmail.com>
2026-02-13 20:40:25 -0500

    audit: migrate acc oversight script to db2pq

--- audit/update_acc_oversight.py
+++ audit/update_acc_oversight.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
-from wrds2pg import wrds_update
+from db2pq import wrds_update_pg as wrds_update
 
 # Auditor Changes
 updated = wrds_update("feed55_auditor_ratification", "audit", 
-                      drop="match: prior: closest: ",
+                      drop="^(match|prior|closest)",
                       col_types={"auditor_ratification_fkey": "integer",
                                  "share_class_fkey": "integer",
                                  "auditor_fkey": "integer",
                                  "pcaob_registration_number": "integer"})
 
 wrds_update("feed56_accounting_estimates_chan", "audit", 
-            drop="match: prior: closest:",
+            drop="^(match|prior|closest)",
             col_types={"accounting_estimates_cha_fke": "integer"})
 
 wrds_update("feed65_impairments", "audit",
-            drop="match: prior: closest: material_impairment_text mtrial_impairment_text_html",
+            drop="^(match|prior|closest|material_impairment_text|mtrial_impairment_text_html)",
             col_types={"mtrl_imprmnt_fct_key": "integer",
                        "mtrl_imprmnt_key": "integer",
                        "quantitative_taxonomy_fkey": "integer",
@@ -23,7 +23,7 @@ wrds_update("feed65_impairments", "audit",
                        "is_final": "boolean"})
 
 #wrds_update("feed74_aqrm", "audit",
-#            drop="match: prior: closest:",
+#            drop="^(match|prior|closest)",
 #            col_types={"flag_year": "integer",
 #                       "fye_of_opinion": "integer",
 #                       "ideal_fye_of_opinion": "integer"})
@@ -37,10 +37,10 @@ wrds_update("feed78_critical_audit_matters", "audit",
 # wrds_update("feed85_cybersecurity", "audit", 
 #             col_types={"cybersecurity_breach_key": "integer",
 #                       "number_of_records_lost": "integer"},
-#             drop="match: prior: closest:")
+#             drop="^(match|prior|closest)")
 
 # wrds_update("feed86_audit_firm_events", "audit",
-#            drop="match: prior: closest:")
+#            drop="^(match|prior|closest)")
 
 wrds_update("feed89_pcaob_report", "audit",
             col_types={"auditor_report_key": "integer",
